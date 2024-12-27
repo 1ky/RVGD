73984f836dd40612cad6fd5cffa74588731f7bcc
Kyle Shepherd <kyshepherd@protonmail.com>
2024-12-26 23:46:00 -0400

    Displaying Genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,24 @@
 import useGenres from "@/hooks/useGenres";
+import getCroppedImage from "@/services/image-url";
+import { HStack, Image, List, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
   const { data } = useGenres();
   return (
-    <ul>
+    <List.Root>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <List.Item key={genre.id} asChild paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              rounded="md"
+              src={getCroppedImage(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </List.Item>
       ))}
-    </ul>
+    </List.Root>
   );
 };
 
