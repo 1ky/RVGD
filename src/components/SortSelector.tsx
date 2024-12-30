86c5cb313aa197b8d066f6c24813d9314981e8e2
Kyle Shepherd <kyshepherd@protonmail.com>
2024-12-30 19:40:49 -0400

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -2,21 +2,42 @@ import { MenuContent, MenuItem, MenuRoot, MenuTrigger } from "@chakra-ui/react";
 import { IoChevronDown } from "react-icons/io5";
 import { Button } from "./ui/button";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Date Added" },
+    { value: "name", label: "Name" },
+    { value: "-released", label: "Release Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Average Rating" },
+  ];
+
+  const currentSortOrder = sortOrders.find(
+    (order) => order.value === sortOrder
+  );
+
   return (
     <MenuRoot positioning={{ placement: "right-start" }}>
       <MenuTrigger asChild>
         <Button variant="outline">
-          Order by: Relevance <IoChevronDown />
+          Order by: {currentSortOrder?.label || "Relevance"} <IoChevronDown />
         </Button>
       </MenuTrigger>
       <MenuContent width={10}>
-        <MenuItem value="relevance">Relevance</MenuItem>
-        <MenuItem value="date-added">Date Added</MenuItem>
-        <MenuItem value="name">Name</MenuItem>
-        <MenuItem value="release-date">Release Date</MenuItem>
-        <MenuItem value="popularity">Popularity</MenuItem>
-        <MenuItem value="average-rating">Average Rating</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem
+            key={order.value}
+            value={order.value}
+            onClick={() => onSelectSortOrder(order.value)}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuContent>
     </MenuRoot>
   );
