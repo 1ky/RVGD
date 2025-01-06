a288c8322f8df1f41671a8bc09fb92191e1cfa0a
Kyle Shepherd <kyshepherd@protonmail.com>
2025-01-05 23:02:14 -0400

    Change bullseye emoji to fire emoji

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,4 +1,4 @@
-import bullsEye from "../assets/bullseye.webp";
+import fire from "../assets/fire.webp";
 import thumbsUp from "../assets/thumbs-up.webp";
 import meh from "../assets/meh.webp";
 import { Image, ImageProps } from "@chakra-ui/react";
@@ -13,7 +13,7 @@ const Emoji = ({ rating }: Props) => {
   const emojiMap: { [key: number]: ImageProps } = {
     3: { src: meh, alt: "meh" },
     4: { src: thumbsUp, alt: "reccomended" },
-    5: { src: bullsEye, alt: "exceptional" },
+    5: { src: fire, alt: "exceptional" },
   };
 
   return <Image {...emojiMap[rating]} boxSize="25px" marginTop={1} />;
