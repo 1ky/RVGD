565aeeb51984facd8c89abff5dd09eb25680339c
Kyle Shepherd <kyshepherd@protonmail.com>
2025-01-24 01:33:22 -0400

    Refactor: Remove unneccessary files and adjusted entities

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import { Provider } from "@/components/ui/provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Provider } from "@/components/ui/provider";
-import { ChakraProvider } from "@chakra-ui/react";
-import { system } from "./theme.ts";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
 import { RouterProvider } from "react-router-dom";
 import router from "./routes.tsx";
 
@@ -13,12 +12,10 @@ const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <ChakraProvider value={system}>
-        <QueryClientProvider client={queryClient}>
-          <RouterProvider router={router} />
-          <ReactQueryDevtools />
-        </QueryClientProvider>
-      </ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
